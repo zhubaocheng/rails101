c9717f83bcec6d43e67f83b0f55f3c61e1cdf94b
zhubaocheng <imzhubaocheng@gmail.com>
2016-12-20 22:42:20 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,12 +5,15 @@ class GroupsController < ApplicationController
   def new
    @group = Group.new
   end
+  def show
+   @group = Group.find(params[:id])
+  end
   def create
    @group = Group.new(group_params)
    @group.save
 
      redirect_to groups_path
- end
+  end
 
  private
 
